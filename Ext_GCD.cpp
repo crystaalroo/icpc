99ee86242aa1d3c7ee45e86f323c2d758620c80e
Aida Crystal <crystal.rodsot@gmail.com>
2021-05-08 01:02:55 -0500

    borrados

--- Ext_GCD.cpp
+++
@@ -1,9 +0,0 @@
-long long extgcd(long long a, long long b, long long &x, long long &y) {
-    if(!b) {
-        x = 1, y = 0;
-        return a;
-    }
-    long long res = extgcd(b, a % b, y, x);
-    y -= a / b * x;
-    return res;
-}
